318096eeb8cd47df59450161849b6420640b0ef1
Kingston's Laptop <42428153+kingstontan@users.noreply.github.com>
2019-03-08 22:49:37 +0800

    first backbone of the app

---
+++ app/src/main/java/com/where2park/where2park/Destination.java
@@ -0,0 +1,18 @@
+package com.where2park.where2park;
+
+public class Destination {
+
+    private String name;
+
+    public Destination(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/com/where2park/where2park/DestinationSelectionActivity.java
+++ app/src/main/java/com/where2park/where2park/DestinationSelectionActivity.java
@@ -1,7 +1,15 @@
 package com.where2park.where2park;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DestinationSelectionActivity extends AppCompatActivity {
 
@@ -9,5 +17,29 @@ public class DestinationSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destination_selection);
+
+        ArrayList<Destination> destinations = new ArrayList<>();
+
+        destinations.add(new Destination("Sunway Pyramid"));
+        destinations.add(new Destination("Sunway University"));
+        destinations.add(new Destination("Monash University"));
+        destinations.add(new Destination("Sunway Geo"));
+        destinations.add(new Destination("Sunway Medical"));
+
+
+
+        ListView destinationList = findViewById(R.id.destinationList);
+
+        DestinationAdapter destinationAdapter = new DestinationAdapter(this, destinations);
+
+        destinationList.setAdapter(destinationAdapter);
+
+        destinationList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(DestinationSelectionActivity.this, ParkingSelectionActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
