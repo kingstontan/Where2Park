3ed02396d1f17e3c45e74e007504da4535515bdd
Linking <baconcrayon@gmail.com>
2019-03-12 14:28:16 +0800

    implemented 2,3,4 until (inserts objects into arrayList in CORRECT sequence) in ParkingSelectionActivity

--- app/src/main/java/com/where2park/where2park/Parking.java
+++ app/src/main/java/com/where2park/where2park/Parking.java
@@ -1,5 +1,68 @@
 package com.where2park.where2park;
 
-public class Parking {
+public class Parking implements Comparable<Parking>{
 
+    private String name;
+    private int lotsavailable;
+    private int etadrive;
+    private int etawalk;
+    private int score;
+
+    public Parking(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLotsavailable() {
+        return lotsavailable;
+    }
+
+    public void setLotsavailable(int lotsavailable) {
+        this.lotsavailable = lotsavailable;
+    }
+
+    public int getEtadrive() {
+        return etadrive;
+    }
+
+    public void setEtadrive(int etadrive) {
+        this.etadrive = etadrive;
+    }
+
+    public int getEtawalk() {
+        return etawalk;
+    }
+
+    public void setEtawalk(int etawalk) {
+        this.etawalk = etawalk;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    //implementation to be updated
+    public void updateRealTimeInfo(){
+        setEtadrive(3);
+        setEtawalk(3);
+        setLotsavailable(500);
+        setScore(3);
+    }
+
+    @Override
+    public int compareTo(Parking o) {
+        return o.getScore() - this.getScore() ;
+    }
 }
--- app/src/main/java/com/where2park/where2park/ParkingSelectionActivity.java
+++ app/src/main/java/com/where2park/where2park/ParkingSelectionActivity.java
@@ -5,12 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+
 public class ParkingSelectionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_selection);
+
+        ArrayList<Parking> sunwayUniversityParkings = new ArrayList<>();
+        sunwayUniversityParkings.add(new Parking("Sunway University Basement"));
+        sunwayUniversityParkings.add(new Parking("BRT Parking"));
+
+
+        String destinationName;
 
         //0. retrieve user's location
 
@@ -21,20 +33,37 @@ public class ParkingSelectionActivity extends AppCompatActivity {
         Bundle extras = intent.getExtras();
 
         if (extras != null) {
-            String destinationName = extras.getString("DESTINATION_NAME");
+            destinationName = extras.getString("DESTINATION_NAME");
 
-//            TextView textView = findViewById(R.id.parkingSelectionTitle);
-//            textView.setText(destinationName);
+            //2. retrieve the appropriate arrayList of places to search
+
+            ArrayList<Parking> parkings = new ArrayList<>();
+
+            switch (destinationName){
+                case "Sunway University":
+                    parkings = sunwayUniversityParkings;
+
+                    default:
+            }
+
+            //3. updates parking objects in the list
+
+            for(Parking p:parkings){
+                p.updateRealTimeInfo();
+            }
+
+            //4. inserts objects into arrayList in CORRECT sequence
+
+            Collections.sort(parkings);
+
         }
 
 
 
 
-        //2. retrieve the appropriate arrayList of places to search
 
-        //3. creates parking objects based on the list
 
-        //4. inserts objects into arrayList in CORRECT sequence
+
 
         //5. create recycler view
 
@@ -45,3 +74,53 @@ public class ParkingSelectionActivity extends AppCompatActivity {
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
